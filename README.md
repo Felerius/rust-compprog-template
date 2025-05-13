49b89e3ae6b965fb2ef3a74d4f1244a55d4f0250
David Stangl <david@david-stangl.com>
2025-05-13 12:16:24 +0200

    Update for 2025

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
 # Rust Template for Competitive Programming
 
-Contains simple input/output code that is convenient to use and ensures that
-you're not bottlenecked by IO.
+Contains simple input & output utilities that are convenient to use and ensures
+that you're not bottlenecked by slow IO performance.
 
 ## Setting up Rust
 
-You'll want to use [Rustup](https://rustup.rs/) to install and manage Rust versions.
-On macOS, you can also use Homebrew to install rustup:
+[Rustup] is by far the most convenient way to install and manage Rust toolchain
+versions. By default, it will use the latest stable release which should be just
+fine for starting out.
 
-```bash
-brew install rustup-init
-rustup-init
-```
+For editor support, there is [rust-analyzer]. It works as a [VsCode extension],
+or generally in anything that supports the Language Server Protocol (vim,
+neovim, etc.). Alternatively, CLion also has Rust support built in.
+
+[Rustup]: https://rustup.rs
+[rust-analyzer]: https://rust-analyzer.github.io/
+[VsCode extension]: https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer
--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,69 @@
 use std::{
     fmt::Debug,
-    io::{self, BufWriter, Lines, StdinLock, StdoutLock, Write},
+    io::{self, BufWriter, StdoutLock, Write},
     str::FromStr,
 };
 
 fn main() {
-    let mut io = Io::new();
+    let mut io = io();
 
-    // Example solution, adapt to your needs
+    // Example solution, adapt to your needs. The read functions from `io` read
+    // whitespace separated values from stdin. Newlines are ignored/treated as
+    // just another form of whitespace.
     let n: usize = io.read();
-    for _ in 0..n {
-        let a: u64 = io.read();
-        let b: u64 = io.read();
-        putln!(io, "{}", a + b);
-    }
+    let ans: u64 = io.read_n::<u64>(n).sum();
+
+    // The `put!` and `putln!` macros correspond to `print!` and `println!`
+    // macros but are more efficient when printing lots of output. If CLion
+    // highlights them macros as errors, you can move the macro definitions from
+    // the end of the file to the top. Both compile fine, it's just CLion being
+    // weird.
+    putln!(io, "{ans}");
 }
 
-struct Io {
-    line: String,
-    offset: usize,
-    lines: Lines<StdinLock<'static>>,
-    writer: BufWriter<StdoutLock<'static>>,
+fn io() -> Io<impl Iterator<Item = &'static str>> {
+    // Leaking the input lines is a bit wasteful but generally fine in a
+    // competitive programming context and avoids having to deal with lifetimes
+    // in the input reading.
+    let input = io::stdin().lines().flat_map(|line| {
+        line.expect("failed to read input line")
+            .leak()
+            .split_whitespace()
+    });
+    Io(input, BufWriter::new(io::stdout().lock()))
 }
+
+struct Io<I>(I, BufWriter<StdoutLock<'static>>);
 
 #[allow(dead_code)]
-impl Io {
-    fn new() -> Self {
-        Self {
-            line: String::new(),
-            offset: 0,
-            lines: io::stdin().lines(),
-            writer: BufWriter::new(io::stdout().lock()),
-        }
+impl<I: Iterator<Item = &'static str>> Io<I> {
+    fn read_str(&mut self) -> &'static str {
+        self.0.next().expect("unexpected end-of-file")
     }
 
-    fn next_token(&mut self) -> &str {
-        loop {
-            if let Some(trim_len) = self.line[self.offset..].find(|c: char| !c.is_whitespace()) {
-                let trimmed = &self.line[self.offset + trim_len..];
-                let len = trimmed.find(char::is_whitespace).unwrap_or(trimmed.len());
-                self.offset += trim_len + len;
-                break &trimmed[..len];
-            }
-
-            self.line = self
-                .lines
-                .next()
-                .expect("unexpected end-of-file")
-                .expect("failed to read input");
-            self.offset = 0;
-        }
+    fn read<T: FromStr<Err: Debug>>(&mut self) -> T {
+        self.read_str().parse().expect("failed to parse input")
     }
 
-    fn read<T: FromStr>(&mut self) -> T
-    where
-        T::Err: Debug,
-    {
-        self.next_token().parse().expect("failed to parse input")
+    fn read_n<T: FromStr<Err: Debug>>(&mut self, n: usize) -> impl Iterator<Item = T> + '_ {
+        (0..n).map(|_| self.read())
     }
 
-    fn collect<T: FromStr, C: FromIterator<T>>(&mut self, len: usize) -> C
-    where
-        T::Err: Debug,
-    {
-        (0..len).map(|_| self.read()).collect()
+    fn collect_n<T: FromStr<Err: Debug>, C: FromIterator<T>>(&mut self, n: usize) -> C {
+        self.read_n(n).collect()
     }
 }
 
 #[macro_export]
 macro_rules! putln {
     ($io:expr $(, $($args:tt)*)?) => {
-        writeln!($io.writer $(, $($args)*)?).expect("failed to write output")
+        writeln!($io.1 $(, $($args)*)?).expect("failed to write output")
     };
 }
 
 #[macro_export]
 macro_rules! put {
     ($io:expr, $($args:tt)*) => {
-        write!($io.writer, $($args)*).expect("failed to write output")
+        write!($io.1, $($args)*).expect("failed to write output")
     };
 }
